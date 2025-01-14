9673b6732e71a20a85cf073e50cadb97c6c571e8
oroldan-dev <oroldan@sksolutions.mx>
2025-01-14 12:23:16 -0600

    feat(output): new outpus

---
+++ output/coc.js
@@ -0,0 +1,20 @@
+import { eq } from '../output/eq.js';
+import { mas } from '../output/mas.js';
+import { mult } from '../output/mult.js'; 
+
+
+      export function coc (x_1,x_2) {
+let z_1 = 0; 
+let aux = x_1; 
+x_1 = 0; 
+
+while ((aux !== 0)) {
+    
+let x_3 = z_1; 
+z_1 = mas( x_3, eq(mas(x_1,1),mas(mult(x_3,x_2), x_2)) ); 
+x_1 ++; 
+aux --; 
+}
+
+return z_1; 
+}
--- output/div.js
+++ output/div.js
@@ -4,18 +4,16 @@ import { mult } from '../output/mult.js';
 
 
       export function div (x_1,x_2) {
-z_1 = 0; 
-let aux = x_1; 
-x_1 = 0; 
+let x_3 = 0; 
+let z_1 = monus(mas(x_1, 1), mas(mult(x_3, x_2), x_2)); 
 
-while ((aux !== 0)) {
+while ((z_1 !== 0)) {
     
-let x_3 = z_1; 
-z_1 = monus(mas(x_1, 1), mas(mult(aux, x_2), x_3)); 
-z_1 ++; 
-x_2 ++; 
-aux --; 
+x_3 ++; 
+z_1 = monus(mas(x_1, 1), mas(mult(x_3, x_2), x_2)); 
 }
+
+z_1 = x_3; 
 
 return z_1; 
 }
