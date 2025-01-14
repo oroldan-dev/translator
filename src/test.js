af64f2822bf812213f7924178089c514876d2d0c
oroldan-dev <oroldan@sksolutions.mx>
2025-01-14 11:19:41 -0600

    fix(translator): imports fixed

--- src/test.js
+++ src/test.js
@@ -1,6 +1,17 @@
 import { mas } from '../output/mas.js'
 import { mult } from '../output/mult.js'
+import { exp } from '../output/exp.js'
+import { fac } from '../output/fac.js'
+import { monus } from '../output/monus.js'
+import { eq } from '../output/eq.js'
+import { div } from '../output/div.js'
 
 console.log("Test")
 console.log("funcion mas", mas(3, 5))
-console.log("funcion mult", mult(4, 4))
+console.log("funcion mult", mult(11, 4))
+console.log("funcion exp", exp(2, 3))
+console.log("funcion fac", fac(4))
+console.log("funcion monus", monus(14, 15))
+console.log("funcion eq", eq(15, 15))
+console.log("funcion eq", eq(1, 15))
+console.log("funcion div", div(16, 3))
--- src/traductor.js
+++ src/traductor.js
@@ -3,22 +3,51 @@ import * as fs from 'fs';
 function translateEssentialToJS(inputFile, outputFile) {
   const essentialCode = fs.readFileSync(inputFile, 'utf-8');
 
-  const importsToAdd = `import { mas } from '../output/mas.js';`
+  let importsToAdd = '';
   const lines = essentialCode.split('\n');
   let insideFunction = false;
   const declaredVariables = new Set();
   const translatedLines = lines.map(line => {
     line = line.trim();
 
-    if (line.startsWith('mas(') || line.startsWith('mult(')) {
+    if (line.startsWith('mas(')) {
+      importsToAdd = ` `
+    } else if (line.startsWith('mult(')) {
+      importsToAdd = `import { mas } from '../output/mas.js';`
+    } else if (line.startsWith('exp(')) {
+      importsToAdd = `import { mult } from '../output/mult.js';`
+    } else if (line.startsWith('fac(')) {
+      importsToAdd = `import { mult } from '../output/mult.js';`
+    } else if (line.startsWith('monus(')) {
+      importsToAdd = `import { pred } from '../output/pred.js';`
+    } else if (line.startsWith('eq(')) {
+      importsToAdd = `import { monus } from '../output/monus.js';
+import { mas } from '../output/mas.js';`
+    } else if (line.startsWith('div(')) {
+      importsToAdd = `import { monus } from '../output/monus.js';
+import { mas } from '../output/mas.js';
+import { mult } from '../output/mult.js'; 
+`
+    }
+
+    if (
+      line.startsWith('mas(')
+      || line.startsWith('mult(')
+      || line.startsWith('exp(')
+      || line.startsWith('fac(')
+      || line.startsWith('monus(')
+      || line.startsWith('pred(')
+      || line.startsWith('eq(')
+      || line.startsWith('div(')
+    ) {
       insideFunction = true;
       const funcName = line.split('(')[0];
       const params = line.match(/\((.*?)\)/)?.[1];
-      return `${importsToAdd} 
-export function ${funcName}(${params}) {`;
+      return `${importsToAdd}
+
+      export function ${funcName} (${params}) {`;
     }
 
-    // End of function
     if (line === '}') {
       if (insideFunction) {
         insideFunction = false;
@@ -30,38 +59,39 @@ export function ${funcName}(${params}) {`;
     if (line.startsWith('clear')) {
       const variable = line.split(' ')[1];
       declaredVariables.add(variable);
-      return `${variable} = 0;`;
+      return `${variable} = 0; `;
     }
     if (line.includes('<-')) {
       const [left, right] = line.split('<-').map(s => s.trim());
       if (insideFunction && !declaredVariables.has(left)) {
         declaredVariables.add(left);
-        return `let ${left} = ${right};`;
+        return `let ${left} = ${right}; `;
       }
-      return `${left} = ${right};`;
+      return `${left} = ${right}; `;
     }
     if (line.startsWith('while')) {
       const condition = line.replace('while', '').replace("}", " ").replace("{", " ").replace('!=', '!==').trim();
-      return `while (${condition}) {`;
+      return `while (${condition}) {
+    `;
     }
     if (line.startsWith('incr')) {
       const variable = line.match(/\((.*?)\)/)?.[1];
-      if (variable) return `${variable}++;`;
+      if (variable) return `${variable} ++; `;
     }
     if (line.startsWith('decr')) {
       const variable = line.match(/\((.*?)\)/)?.[1];
-      if (variable) return `${variable}--;`;
+      if (variable) return `${variable} --; `;
     }
     if (line.startsWith('return')) {
       const variable = line.split(' ')[1];
-      return `return ${variable};`;
+      return `return ${variable}; `;
     }
     return line;
   });
 
   const translatedCode = translatedLines.join('\n');
   fs.writeFileSync(outputFile, translatedCode, 'utf-8');
-  console.log(`Translation complete. Code saved to ${outputFile}`);
+  console.log(`Translation complete.Code saved to ${outputFile} `);
 }
 
 const args = process.argv.slice(2);
@@ -77,7 +107,7 @@ const outputFile = `output/${functionName}.js`;
 // Remove the file if it exists
 if (fs.existsSync(outputFile)) {
   fs.unlinkSync(outputFile);
-  console.log(`Removed existing file: ${outputFile}`);
+  console.log(`Removed existing file: ${outputFile} `);
 }
 
 translateEssentialToJS(inputFile, outputFile);
